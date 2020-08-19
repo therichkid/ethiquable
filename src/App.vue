f49492f7a6d3429ff047e54b903013d60b008acd
Richard Zille <therichkid@gmx.de>
2020-08-19 10:22:09 +0200

    Initial commit from bayciv repository, edit metadata, remove vue-analytics

---
+++ src/App.vue
@@ -0,0 +1,132 @@
+<template>
+  <v-app>
+    <NavigationDrawer ref="drawer" />
+
+    <AppBar @toggleClicked="onToggleEmit()" />
+
+    <v-main>
+      <v-container fluid class="main-container">
+        <router-view />
+      </v-container>
+      <Footer />
+    </v-main>
+
+    <FAB />
+
+    <CookieAlert />
+  </v-app>
+</template>
+
+<script>
+import NavigationDrawer from "@/components/layouts/NavigationDrawer";
+import AppBar from "@/components/layouts/AppBar";
+import Footer from "@/components/layouts/Footer";
+import FAB from "@/components/partials/FAB";
+import CookieAlert from "@/components/partials/CookieAlert";
+
+export default {
+  components: { NavigationDrawer, AppBar, Footer, FAB, CookieAlert },
+
+  methods: {
+    onToggleEmit() {
+      this.$refs.drawer.drawer = !this.$refs.drawer.drawer;
+    }
+  },
+
+  mounted() {
+    // Workaround to disable ripple effect on touchmove
+    window.addEventListener(
+      "touchmove",
+      () => {
+        document.body.classList.add("hide-ripple");
+      },
+      { passive: true }
+    );
+    window.addEventListener("touchend", () => {
+      setTimeout(() => {
+        document.body.classList.remove("hide-ripple");
+      }, 50);
+    });
+  }
+};
+</script>
+
+<style>
+.main-container {
+  max-width: 1200px;
+  min-height: 60vh;
+}
+.grecaptcha-badge {
+  display: none;
+}
+.post-container {
+  max-width: 750px;
+  line-height: 1.8;
+  word-wrap: break-word;
+  hyphens: auto;
+}
+.post-container p {
+  text-align: justify;
+}
+.event-card {
+  width: 100%;
+  min-width: 200px;
+  max-width: 275px;
+}
+.v-card__title {
+  word-break: break-word;
+}
+.v-banner__wrapper {
+  border-bottom: none !important;
+}
+.hide-ripple .v-ripple__container {
+  display: none;
+}
+/* WP classes */
+/* Inherited via v-html so they need a global style to be attached */
+[class*="wp-image"],
+.wp-caption {
+  height: auto;
+  max-width: 100%;
+}
+.alignleft {
+  float: left;
+  margin: 0 10px 0 0;
+  max-width: 100%;
+}
+.aligncenter {
+  display: block;
+  margin: 0 auto;
+  max-width: 100%;
+}
+.alignright {
+  float: right;
+  margin: 0 0 0 10px;
+  max-width: 100%;
+}
+.alignnone {
+  margin: 0;
+  max-width: 100%;
+}
+@media screen and (max-width: 599px) {
+  .alignright,
+  .alignleft {
+    float: none;
+    display: block;
+    margin: 0 auto;
+    max-width: 100%;
+  }
+}
+.tablepress {
+  width: 100%;
+  display: table;
+  overflow-x: auto;
+  overflow-y: hidden;
+}
+.tablepress th {
+  font-size: unset !important;
+}
+iframe {
+  max-width: 100%;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,32 @@
+import "babel-polyfill";
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import router from "./router/index";
+import store from "./store/index";
+
+import shared from "./services/shared";
+Vue.prototype.shared = shared;
+
+// reCAPTCHA
+import { VueReCaptcha } from "vue-recaptcha-v3";
+Vue.use(VueReCaptcha, { siteKey: "6LfQl6UUAAAAACpOBLqwIOQso7e1hGWdDiH_CbwJ" });
+
+// vue2-leaflet
+import { Icon } from "leaflet";
+import "leaflet/dist/leaflet.css";
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+});
+
+Vue.config.productionTip = false;
+
+new Vue({
+  vuetify,
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
