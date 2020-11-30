fb1d93eecc47ace0c940f7d8cdfd9293a571736a
Richard Zille <therichkid@gmx.de>
2020-11-30 20:03:38 +0100

    Fetch specialized trade page from WP now

--- src/views/SpecializedTrade.vue
+++ src/views/SpecializedTrade.vue
@@ -1,65 +1,19 @@
 <template>
   <v-container class="post-container">
-    <v-row align="center">
+    <LoadingSkeleton v-if="isLoading" />
+    <LoadingError v-if="loadingError" :height="500" @retryAgain="getPage()" />
+
+    <v-row v-if="!isLoading && !loadingError && Object.keys(page).length" align="center">
       <!-- Header -->
       <v-col cols="12">
-        <h1 class="text-h4">Fachhandel</h1>
+        <h1 class="text-h4">{{ page.title }}</h1>
       </v-col>
 
-      <!-- Order offline -->
-      <v-col cols="12">
-        <h2 class="text-h5 mb-2">Bestellung per Fax / Brief / E-Mail</h2>
-        <div>
-          Senden Sie uns das Bestellformular per Fax / Brief / E-Mail zusammen mit Ihrer Gewerbeanmeldung. Klicken Sie
-          auf den Button, um das Formular herunterzuladen.
-        </div>
-        <div class="trade-button mt-4">
-          <v-btn
-            block
-            tile
-            large
-            color="secondary"
-            :href="publicPath + 'img/bestellformular-fachhandel.pdf'"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Bestellformular für Fax & Brief
-            <v-spacer></v-spacer>
-            <v-icon right>mdi-download</v-icon>
-          </v-btn>
-        </div>
-      </v-col>
+      <!-- Body -->
+      <v-col cols="12" v-html="page.content" class="wp-content pb-0"></v-col>
 
-      <!-- Order online -->
-      <v-col cols="12">
-        <h2 class="text-h5 mb-2">Online-Bestellung</h2>
-        <div>
-          Die Mindestbestellmenge für Frei-Haus-Lieferungen beträgt lediglich 100€.
-          <br />
-          Wenn Sie schon Ihre Zugangsdaten (Kundennummer und Kennwort) haben, dann tragen Sie diese bitte in das
-          Online-Bestellformular ein.
-        </div>
-        <div class="trade-button mt-4">
-          <v-btn
-            block
-            tile
-            large
-            color="secondary"
-            href="https://www.ethiquablebestellung.de/bestellung/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Zum Online-Bestellformular
-            <v-spacer></v-spacer>
-            <v-icon right>mdi-open-in-new</v-icon>
-          </v-btn>
-        </div>
-
-        <!-- Sign-up -->
-        <div class="mt-4">
-          Sollten Sie noch keine Zugangsdaten haben, tragen Sie Ihren Namen und E-Mail-Adresse hier ein und wir schicken
-          sie Ihnen gern.
-        </div>
+      <!-- Form -->
+      <v-col cols="12" class="pt-0">
         <v-form v-model="valid">
           <v-text-field label="Name" v-model="name" required></v-text-field>
           <v-text-field label="E-Mail" v-model="email" :rules="emailRules" required></v-text-field>
@@ -109,16 +63,20 @@
 
 <script>
 import api from "@/services/api";
+import LoadingSkeleton from "@/components/partials/LoadingSkeleton";
+import LoadingError from "@/components/partials/LoadingError";
 import AlertModal from "@/components/partials/AlertModal";
 
 export default {
   components: {
+    LoadingSkeleton,
+    LoadingError,
     AlertModal
   },
 
   data() {
     return {
-      publicPath: process.env.BASE_URL,
+      page: {},
       valid: false,
       name: "",
       email: "",
@@ -131,7 +89,43 @@ export default {
     };
   },
 
+  computed: {
+    isLoading() {
+      return this.$store.state.pageLoading;
+    },
+    loadingError() {
+      return this.$store.state.pageLoadingError;
+    },
+    failedRequests() {
+      return this.$store.state.failedRequests;
+    }
+  },
+
+  watch: {
+    page(page) {
+      if (!page && !this.failedRequests) {
+        this.$router.push("/404");
+      }
+    }
+  },
+
   methods: {
+    async getPage() {
+      const slug = "fachhandel";
+      const pageFetched = this.$store.getters.getFetchedPageBySlug(slug);
+      if (pageFetched[0]) {
+        // Already fetched
+        this.page = pageFetched[1];
+      } else {
+        // Not fetched yet
+        this.page = await this.$store.dispatch("fetchPageBySlug", slug).catch(error => {
+          console.error(error);
+        });
+      }
+      if (this.page) {
+        document.title = this.page.title + " - " + document.title;
+      }
+    },
     async sendForm() {
       const data = {
         fullname: this.name.trim(),
@@ -153,13 +147,45 @@ export default {
           this.alertMessage = error;
         });
       this.dialog = true;
+    },
+    goBack() {
+      window.history.length > 1 ? this.$router.go(-1) : this.$router.push("/");
     }
+  },
+
+  created() {
+    this.getPage();
   }
 };
 </script>
 
 <style scoped>
-.trade-button {
+* >>> h3,
+* >>> h4,
+* >>> h5,
+* >>> h6 {
+  color: var(--v-primary-base);
+  text-transform: uppercase;
+}
+.wp-content >>> a {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
   max-width: 500px;
+  height: 44px;
+  padding: 0 20px;
+  background-color: var(--v-secondary-base);
+  color: #fff !important;
+  font-size: 14px;
+  font-weight: 500;
+  letter-spacing: 1.25px;
+  text-decoration: none;
+  text-transform: uppercase;
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 3px 1px -2px, rgba(0, 0, 0, 0.14) 0px 2px 2px 0px,
+    rgba(0, 0, 0, 0.12) 0px 1px 5px 0px;
+}
+.wp-content >>> a > i {
+  font-size: 18px;
 }
 </style>
